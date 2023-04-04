ee947e0d645c3ba90bbeba273227f668b8fe4d12
heedaelee <adguy72@gmail.com>
2023-04-04 15:45:58 +0900

    reduce 추가

---
+++ reduce.js
@@ -0,0 +1,22 @@
+const oneTwoThree = [1, 2, 3];
+
+let result = oneTwoThree.reduce((acc, cur, i) => {
+  // console.log(acc, cur, i);
+  return acc + cur;
+}, 0);
+// console.log(result);
+
+
+const promiseFactory = (time) => {
+  return new Promise((resolve, reject) => {
+    console.log(time);
+    setTimeout(resolve, time);
+  });
+};
+[1000, 2000, 3000, 4000].reduce((acc, cur) => {
+  return acc.then(() => promiseFactory(cur));
+}, Promise.resolve());
+
+/** 
+ * 출 : https://www.zerocho.com/category/JavaScript/post/5acafb05f24445001b8d796d
+*/
